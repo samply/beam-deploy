ea16d0016fd2a0a52ca3e06483295d5987ebf5b1
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2023-02-14 13:32:36 +0100

    Fix create_privkey_and_cert.sh

--- pki-scripts/create_privkey_and_cert.sh
+++ pki-scripts/create_privkey_and_cert.sh
@@ -32,10 +32,13 @@ source "$BASE_DIR/.env"
 VAULT_TOKEN=$(cat $BASE_DIR/pki/pki.secret)
 [[ -z $VAULT_ADDR ]] && ( echo "VAULT_ADDR not set! Please check your .env file."; exit 1)
 
-if [[ -z $1 ]]; then
-  echo "Usage: $0 <proxyname>"
-else
-  request_proxy $1
-  mkdir certificates
-  cp -v pki/$1*priv* certificates/
+if [[ -z ${1:-} ]]; then
+  echo "Usage: $0 <proxy_shortname>"
+  echo
+  echo "Note: This tool should not be used for registering beam proxies. Instead, use beam-enroll to create a CSR and managepki (https://hub.docker.com/r/samply/managepki) to accept it."
+  exit 1
 fi
+
+request_proxy $1
+mkdir -p certificates
+cp -v pki/$1*priv* certificates/
